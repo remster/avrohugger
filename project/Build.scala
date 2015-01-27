d40ac2d96a7390df275987b822d2640366cdbd9a
julianpeeters <julianpeeters@gmail.com>
2015-01-26 22:15:26 -0800

    release 0.0.1 for scala 2.10

--- project/Build.scala
+++ project/Build.scala
@@ -5,9 +5,9 @@ import sbtassembly.AssemblyPlugin.autoImport._
 object BuildSettings {
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "com.julianpeeters",
-    version := "0.0.2-SNAPSHOT",
+    version := "0.0.1",
     scalacOptions ++= Seq(),
-    scalaVersion := "2.11.5",
+    scalaVersion := "2.10.4",
     libraryDependencies += "org.apache.avro" % "avro" % "1.7.7",
     libraryDependencies += "org.specs2" %% "specs2" % "2.4" % "test",
     parallelExecution in Test := false,
